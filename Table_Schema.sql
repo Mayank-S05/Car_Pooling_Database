9a59c7a876ce57f9d40861b9cd95ebef5eb81034
Mayank_S05 <mayank.singh0597@gmail.com>
2017-05-12 17:11:29 +0530

    Update Table_Schema.sql

--- Table_Schema.sql
+++ Table_Schema.sql
@@ -1,89 +1,105 @@
 
 CREATE TABLE Security_Question
 (
-  Q_ID INT NOT NULL,
-  Question INT NOT NULL,
+  Q_ID INT(4) NOT NULL,
+  Question varchar(100) NOT NULL,
   PRIMARY KEY (Q_ID)
 );
 
-CREATE TABLE User
+
+CREATE TABLE users
 (
-  E_Mail INT NOT NULL,
-  Password INT NOT NULL,
-  Name INT NOT NULL,
-  Q_ID INT NOT NULL,
+  E_Mail varchar(100) NOT NULL,
+  Passwords INT(20) NOT NULL,
+  Name_users varchar(30) NOT NULL,
+  Q_ID INT(4) NOT NULL,
   PRIMARY KEY (E_Mail),
   FOREIGN KEY (Q_ID) REFERENCES Security_Question(Q_ID)
 );
 
+
 CREATE TABLE Traveller
 (
-  Source INT NOT NULL,
-  Destination INT NOT NULL,
-  Dept_Time INT NOT NULL,
-  Nos_of_Passengers INT NOT NULL,
-  date INT NOT NULL,
-  T_ID INT NOT NULL,
-  E_Mail INT NOT NULL,
-  PRIMARY KEY (T_ID),
-  FOREIGN KEY (E_Mail) REFERENCES User(E_Mail)
+
+  SourceD varchar(100) NOT NULL,
+  Destination varchar(100) NOT NULL,
+  Dept_Time varchar(10) NOT NULL,
+  Nos_of_Passengers INT(20) NOT NULL,
+  dateof date  NOT NULL ,
+  T_ID INT(4) NOT NULL,
+  T_E_Mail VARCHAR(20) not null REFERENCES Users(E_Mail),
+  PRIMARY KEY (T_ID)
 );
 
-CREATE TABLE Owner
+
+CREATE TABLE owners
 (
-  Company_Name INT,
-  O_ID INT NOT NULL,
-  E_Mail INT NOT NULL,
-  PRIMARY KEY (O_ID),
-  FOREIGN KEY (E_Mail) REFERENCES User(E_Mail)
+  Company_Name varchar(100),
+  O_ID INT(5) NOT NULL,
+  O_E_Mail VARCHAR(20) not null REFERENCES Users(E_Mail),
+  PRIMARY KEY (O_ID)
 );
+
 
 CREATE TABLE Auto_Service
 (
-  Music_Choice INT NOT NULL,
-  Plate_Nos INT NOT NULL,
-  Charges INT NOT NULL,
-  Date INT NOT NULL,
-  A_ID INT NOT NULL,
-  Time INT NOT NULL,
-  O_ID INT NOT NULL,
-  PRIMARY KEY (A_ID),
-  FOREIGN KEY (O_ID) REFERENCES Owner(O_ID)
+  A_ID INT(20) NOT NULL,
+  Music_Choice varchar(100) NOT NULL,
+  Plate_Nos varchar(20) NOT NULL,
+  Charges INT(10) NOT NULL,
+  timeof1 varchar(10)  NOT NULL,
+  dateof1 date NOT NULL,
+  A_O_ID VARCHAR(20) not null REFERENCES Users(O_ID),
+  PRIMARY KEY (A_ID)
 );
 
-CREATE TABLE Car_Service
+
+CREATE TABLE Car_Details
 (
-  Date INT NOT NULL,
-  Time INT NOT NULL,
-  Service_id INT NOT NULL,
-  Charges INT NOT NULL,
-  O_ID INT NOT NULL,
-  PRIMARY KEY (Service_id),
-  FOREIGN KEY (O_ID) REFERENCES Owner(O_ID)
+  Plate_Nos varchar(20) NOT NULL,
+  Color varchar(10) NOT NULL,
+  AC_NonAC varchar(20) NOT NULL,
+  Model varchar(20) NOT NULL,
+  Capacity INT(20) NOT NULL,
+  PRIMARY KEY (Plate_Nos),
+  C_S_ID VARCHAR(20) not null REFERENCES Car_Service(Service_id)
 );
+
 
 CREATE TABLE Preferences
 (
-  Music INT NOT NULL,
-  Smoking INT NOT NULL,
-  Gossip INT NOT NULL,
-  Gender INT NOT NULL,
-  Service_id INT NOT NULL,
-  FOREIGN KEY (Service_id) REFERENCES Car_Service(Service_id)
+  Music varchar(20) NOT NULL,
+  Smoking varchar(20) NOT NULL,
+  Gossip varchar(20) NOT NULL,
+  Gender varchar(10) NOT NULL,
+  P_S_ID VARCHAR(20) not null REFERENCES Car_Service(Service_id)
 );
+
 
 CREATE TABLE Route
 (
-  Source INT NOT NULL,
-  Mile_Stones INT NOT NULL,
-  OneWay_TwoWay INT,
-  Capacity INT NOT NULL,
-  Destination INT NOT NULL,
-  A_ID INT NOT NULL,
-  Service_id INT NOT NULL,
-  FOREIGN KEY (A_ID) REFERENCES Auto_Service(A_ID),
-  FOREIGN KEY (Service_id) REFERENCES Car_Service(Service_id)
+  R_ID varchar(20) NOT NULL,
+  SourceD varchar(100) NOT NULL,
+  Mile_Stones varchar(100) NOT NULL,
+  OneWay_TwoWay varchar(10) ,
+  Capacity INT(20) NOT NULL,
+  Destination varchar(100) NOT NULL,
+  R_A_ID VARCHAR(20) not null REFERENCES Auto_Service(A_ID),
+  S_S_ID VARCHAR(20) not null REFERENCES Car_Service(Service_id),
+  PRIMARY KEY(R_ID)
 );
+ 
+ CREATE TABLE Car_Service
+(
+  timeof2 varchar(50) NOT NULL,
+  dateof2 Date  NOT NULL,
+  Service_id INT(10) NOT NULL,
+  Charges INT(10) NOT NULL,
+  PRIMARY KEY (Service_id),
+  C_O_ID VARCHAR(20) not null REFERENCES Users(O_ID),
+  C_T_ID VARCHAR(20) not null REFERENCES Traveller(T_ID)
+);
+
 
 CREATE TABLE Car_Details
 (
